dc8ad536f0be24455e0e7246d3bc2e388915f729
Dan Hogan <hogan.daniel@hotmail.com>
2019-04-30 10:30:39 -0700

    Added label to range slider to display value

--- src/application.js
+++ src/application.js
@@ -1,6 +1,13 @@
 import getBoard from './getBoard.js';
 const form = document.getElementById('boardForm');
 
+const boardQuality = document.getElementById('boardQuality');
+const boardQualityLabel = document.getElementById('boardQualityLabel');
+
+boardQuality.addEventListener('change', function() {
+    boardQualityLabel.textContent = boardQuality.value + '/10';
+});
+
 form.addEventListener('submit', event => {
     event.preventDefault();
     const formData = new FormData(form);
